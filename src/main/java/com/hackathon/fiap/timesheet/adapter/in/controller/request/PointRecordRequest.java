d90742918c2a94ef2b235c123e131a01aaff9523
Wesley Gomes <wesleygomes197@gmail.com>
2024-03-23 15:10:52 -0300

    Melhorias no projeto

--- src/main/java/com/hackathon/fiap/timesheet/adapter/in/controller/request/PointRecordRequest.java
+++ src/main/java/com/hackathon/fiap/timesheet/adapter/in/controller/request/PointRecordRequest.java
@@ -1,5 +1,6 @@
 package com.hackathon.fiap.timesheet.adapter.in.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hackathon.fiap.timesheet.application.core.contants.PointRecordType;
 import lombok.*;
 
@@ -13,7 +14,9 @@ import java.time.LocalTime;
 @NoArgsConstructor
 public class PointRecordRequest {
     private Long employeeId;
+    @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate date;
+    @JsonFormat(pattern="HH:mm")
     private LocalTime time;
     private PointRecordType type;
 }
